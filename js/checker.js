f9a91be4415cb2c25ea60817988bb4f6a169e707
Yi-Wei Chen <feberium@gmail.com>
2014-05-09 21:00:42 +0800

    Last submission of practice
    In the practice, the app will require user log in to get user's groups.
    and it will span your groups to report spam clubs for you. If you like
    the apps, you can like the app in the botom of the page for support me.
    Thanks!

--- js/checker.js
+++ js/checker.js
@@ -32,7 +32,7 @@ $.getJSON('http://jsbin.com/jaziroja/1',{}, function(data) {
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '259195264246285', // 若可以，請換成自己的 App ID !
+    appId: '1432101037043765', // 若可以，請換成自己的 App ID !
     status: true
   });
 
@@ -43,9 +43,20 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
-    // 2. 以 FB.api 拿到使用者的 group 列表
-    // 拿到使用者 group 列表的 response 之後：
-    // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
-
+      FB.login(function(){
+        console.log("login");
+        // 2. 以 FB.api 拿到使用者的 group 列表
+        FB.api('/me/groups',function(resp){
+          console.log('User groups:',resp.data);
+        });        
+        // 拿到使用者 group 列表的 response 之後：
+        for (var i = 0; i < resp.data.length; i++) {
+          group = resp.data[i];
+          if( junkGroups.indexOf(group.id) !== -1 ){
+            results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>')
+          }
+        }
+        results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');      
+      }, {scope: 'user_groups'});    
   });
 };
